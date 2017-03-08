54d77b80c8c55c1b461f724cb336cabbc18b179f
Aleks <aleks.stefanovic101@gmail.com>
2017-03-07 23:07:11 -0500

    with ads and contact us and better auto scaling

--- Pekol/Site.Master.designer.cs
+++ Pekol/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Pekol {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// AdRotator control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.AdRotator AdRotator;
+        
+        /// <summary>
         /// SiteMapPath control.
         /// </summary>
         /// <remarks>
